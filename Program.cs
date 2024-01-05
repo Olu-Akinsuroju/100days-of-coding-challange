dc125ed6b943150f9a753efb1eab19516344db19
Olu-Akinsuroju <155703743+Olu-Akinsuroju@users.noreply.github.com>
2024-01-05 19:32:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _100days_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please type in your name");
+            String readResult = Console.ReadLine();
+            Console.WriteLine($"Thank you {readResult} for being a part of my 100 days c# code challange");
+            Console.WriteLine(" see you tommorow");
+        }
+    }
+}
