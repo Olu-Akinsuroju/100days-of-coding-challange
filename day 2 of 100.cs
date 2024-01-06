1395d48905d29eb14685bf7a574266c260aec47d
Olu-Akinsuroju <155703743+Olu-Akinsuroju@users.noreply.github.com>
2024-01-06 23:55:19 +0100

    Add files via upload

---
+++ day 2 of 100.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _100days_c_
+{
+    internal class day_2_of_100
+    {
+        static void Main(string[] args)
+        {
+            //this program ask for a temperature degree which it covert to calcius and ferhant
+            Console.WriteLine("type in a temperature degree");
+            string readResult = Console.ReadLine();
+            if (double.TryParse(readResult, out double number))
+            {
+                double fahrenheit = (number * 9 / 5) + 32;
+                double celcius = (number - 32) * 5 / 9;
+
+                //the output
+                Console.WriteLine($"the convation of temprature {readResult} degree is {fahrenheit} in fahrenheit and {celcius} degree in celcius");
+            }
+            else { Console.WriteLine(" invalid input"); }
+        }
+    }
+}
